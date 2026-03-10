07352a80dffd9a5260bcfc6dd14630338deb0346
Flavio de Oliveira Santos <fos.operacoes@gmail.com>
2026-03-10 00:27:39 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 //  [4] Limpeza de caches antigos no activate
 // ============================================================
 
-const CACHE_VERSION = 'op245-c-v7.7';
+const CACHE_VERSION = 'op245-c-v7.9';
 const STATIC_CACHE  = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const ALL_CACHES    = [STATIC_CACHE, DYNAMIC_CACHE];
